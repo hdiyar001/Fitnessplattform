6c04084a44fa3193b00ecef49562965b3eb004ac
h_diyar <hdiyar998@gmail.com>
2023-08-24 14:49:56 +0200

    test

--- src/fitnessplattform/projekt/FitnessPlattformProjekt.java
+++ src/fitnessplattform/projekt/FitnessPlattformProjekt.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
 package fitnessplattform.projekt;
 
 import javafx.application.Application;
@@ -14,27 +10,28 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author 49179
+ * @author << Diyar >>
  */
 public class FitnessPlattformProjekt extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
+        System.out.println("ttttttttttttttttttttttttttttttt");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
         });
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -46,5 +43,5 @@ public class FitnessPlattformProjekt extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
