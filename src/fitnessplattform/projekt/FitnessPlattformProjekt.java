f8ac5e8cf1f6ffb4fbeb4f4398fde69235ee41a3
HDiyar <hdiyar998@gmail.com>
2023-08-24 16:01:06 +0200

    lastchange to test

--- src/fitnessplattform/projekt/FitnessPlattformProjekt.java
+++ src/fitnessplattform/projekt/FitnessPlattformProjekt.java
@@ -23,28 +23,6 @@ public class FitnessPlattformProjekt extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-                System.out.println("Hello World!");
-
             }
         });
 
