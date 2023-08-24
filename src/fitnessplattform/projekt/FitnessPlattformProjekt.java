f82f5b4b62aad2fec073c87a5cbb9f814db6764e
HDiyar <hdiyar998@gmail.com>
2023-08-24 15:18:17 +0200

    tesetss

--- src/fitnessplattform/projekt/FitnessPlattformProjekt.java
+++ src/fitnessplattform/projekt/FitnessPlattformProjekt.java
@@ -23,6 +23,8 @@ public class FitnessPlattformProjekt extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
+                System.out.println("Hello World!");
+                System.out.println("Hello World!");
             }
         });
 
